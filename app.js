7fc1a8d39f49425e9ece145d12337ab250718867
Martin N <mr.avgp@googlemail.com>
2014-10-15 13:30:23 +0000

    Appending forecast to container

--- app.js
+++ app.js
@@ -120,17 +120,20 @@ navigator.geolocation.getCurrentPosition(function(pos) {
     weatherContent.querySelector('h2').textContent = weather.weather[0].description;
     var forecastContainer = weatherContent.querySelector(".forecast");
 
+    clearChildren(container);
+
     getForecastForLocation(location, function(forecasts) {
       clearChildren(forecastContainer);
       var i = forecasts.length,
-          container = document.importNode(document.querySelector('template#forecast_entry').content, true);
+          fcContainer = document.importNode(document.querySelector('template#forecast_entry').content, true);
 
       while(i--) {
         var date = new Date(parseInt(forecasts[i].dt, 10) * 1000);
-        container.querySelector('.forecast_time').textContent = date.toLocaleTimeString().slice(0, -3);
-        container.querySelector('.forecast_img img').src = 'http://openweathermap.org/img/w/' + forecasts[i].weather[0].icon + '.png';
-        container.querySelector('.forecast_temp').textContent = forecasts[i].main.temp;
-        container.querySelector('.forecast_text').textContent = forecasts[i].weather[0].description;
+        fcContainer.querySelector('.forecast_time').textContent = date.toLocaleTimeString().slice(0, -3);
+        fcContainer.querySelector('.forecast_img img').src = 'http://openweathermap.org/img/w/' + forecasts[i].weather[0].icon + '.png';
+        fcContainer.querySelector('.forecast_temp').textContent = forecasts[i].main.temp;
+        fcContainer.querySelector('.forecast_text').textContent = forecasts[i].weather[0].description;
+        container.appendChild(fcContainer);
       }
     });
 
@@ -141,7 +144,6 @@ navigator.geolocation.getCurrentPosition(function(pos) {
     } catch(e) {
       document.head.querySelector('title').textContent = mainWeather.temp + ' °C, ' + weather.weather[0].description;
     }
-    clearChildren(container);
     container.appendChild(weatherContent);
   });
 });
